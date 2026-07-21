21a13acc92a9890168fa74e495cf5cc625e63ed7
evkondr <kondratiev.evg@gmail.com>
2026-07-21 10:53:10 +0300

    feat: add jwt tool on server

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config();
+
+const ACCESS_SECRET = process.env.ACCESS_TOKEN as string;
+const REFRESH_SECRET = process.env.REFRESH_TOKEN as string;
+
+export const createAccessToken = (userId: string) => {
+  return jwt.sign({ userId }, ACCESS_SECRET, { expiresIn: "15m" });
+};
+export const createRefreshToken = (userId: string) => {
+  return jwt.sign({ userId }, REFRESH_SECRET, { expiresIn: "5d" });
+};
+export const verifyAccessToken = (token: string) => {
+  return jwt.verify(token, ACCESS_SECRET);
+};
+export const verifyRefreshToken = (token: string) => {
+  return jwt.verify(token, REFRESH_SECRET);
+};
