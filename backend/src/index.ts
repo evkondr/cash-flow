ee2159bf3de11081922fe9243762853dfd34c4d3
evkondr <kondratiev.evg@gmail.com>
2026-09-07 10:41:43 +0300

    feat: move global.d.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,10 +28,3 @@ app.get("/", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-declare global {
-  namespace Express {
-    interface Request {
-      userId: number;
-    }
-  }
-}
